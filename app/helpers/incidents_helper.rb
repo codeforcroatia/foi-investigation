8127129bf55fa6e5845edc47d92f05ae23e381cb
Miroslav Schlossberg <mschlos@gmail.com>
2016-03-30 23:44:11 +0200

    customization url

--- app/helpers/incidents_helper.rb
+++ app/helpers/incidents_helper.rb
@@ -4,16 +4,15 @@ module IncidentsHelper
   # with incident specific details. It also populates the form with tags that
   # can later be used for tracking requests
   def link_to_right_to_know_form(incident, text=nil)
-    url = "http://imamopravoznati.org/new/#{ incident.location.geoloc }/?title=FOI%20Request%20for%20Detail%20Incident%20Report%20#{incident.incident_number};" +
-      "body=To%20the%20Department%20of%20Immigration%20and%20Border%20Protection%2C%0A%0ADear%20Sir%2FMadam" +
-      "%2C%0A%0AUnder%20the%20Freedom%20of%20Information%20Act%201982%20(Cth)%20I%20request%20the%20" +
-      "following%20document%3A%0A%0AIncident%20Detail%20Report%20#{ incident.incident_number }%20" +
-      "from%20the%20Department%27s%20Compliance%2C%20Case%20Management%2C%20Detention%20and%20" +
-      "Settlement%20Portal.%20I%20also%20request%20any%20documents%20attached%20to%20the%20detailed" +
-      "%20report.%0A%0AKind%20Regards%2C%0A%0A****ADD%20YOUR%20NAME%20HERE%20BEFORE%20SENDING%20REQUEST****" +
-      "&tags=" + u("detentionlogs incident-number:#{incident.incident_number}")
-    text ||= "Adopt Incident #{incident.incident_number}"
-    title =  "Opens a prefilled Freedom of Information request using the Right To Know FOI tool"
+    url = "http://imamopravoznati.org/new/#{ incident.location.geoloc }/?title=#{incident.incident_number};" +
+      "body=Za:%20#{ incident.location.name }%2C%0A%0APo%C5%A1tovani%2C%0A%0A" +
+      "%2C%0A%0APod%20Zakonom%20o%20pravu%20na%20pristup%20informacijama%20molim%20vas%20dostavite%20ljede%C4%87e%20" +
+      "informacije%3A%0A%0A%20#{ incident.incident_description }.%20" +
+      "Dostavite%20na%20ovu%20adresu%20u%20digitalnom%20obliku%20." +
+      "%0A%0ALijep%20pozdrav%2C%0A%0A****OVDJE%20NAVEDITE%20IME%20I%20PREZIME%20PRIJE%20SLANJA%20ZAHTJEVA****" +
+      "&tags=" + u("foi-investigation foi-number:#{incident.incident_number}")
+    text ||= "Preuzmi #{incident.incident_number}"
+    title =  "Otvara unaprijed popunjeni zahtjev za pristup informacijama kori<C5>tenjem Imamo pravo znati alata"
     link_to text, url, { title: title, class: "btn btn-submit"}
   end
 
@@ -28,11 +27,11 @@ module IncidentsHelper
   def link_to_right_to_know(incident)
     if (incident.foi_requests.count == 1)
       foi_request = incident.foi_requests.first
-      text = "FOI Request for more detail: #{foi_request.display_status}"
+      text = "PPI zahtjev za vi<C5>e detalja: #{foi_request.display_status}"
       link = "http://imamopravoznati.org/request/#{foi_request.url_title}"
     else
       statuses = incident.foi_requests.map(&:display_status).uniq
-      text = "#{incident.foi_requests.length} FOI Requests for more detail: #{statuses.to_sentence.gsub('.', '')}"
+      text = "#{incident.foi_requests.length} PPI zahtjev za vi<C5>e detalja: #{statuses.to_sentence.gsub('.', '')}"
       link = "http://imamopravoznati.org/search/#{incident.incident_number}"
     end
 
