538d6a2fe19d570ab04bab063509fb8ce07dd59c
Miroslav Schlossberg <mschlos@gmail.com>
2016-04-03 21:53:07 +0200

    changes in url

--- app/helpers/incidents_helper.rb
+++ app/helpers/incidents_helper.rb
@@ -4,15 +4,15 @@ module IncidentsHelper
   # with incident specific details. It also populates the form with tags that
   # can later be used for tracking requests
   def link_to_right_to_know_form(incident, text=nil)
-    url = "http://imamopravoznati.org/new/#{ incident.location.geoloc }/?title=#{incident.incident_number};" +
-      "body=Za:%20#{ incident.location.name }%2C%0A%0APo%C5%A1tovani%2C%0A%0A" +
-      "%2C%0A%0APod%20Zakonom%20o%20pravu%20na%20pristup%20informacijama%20molim%20vas%20dostavite%20ljede%C4%87e%20" +
-      "informacije%3A%0A%0A%20#{ incident.incident_description }.%20" +
-      "Dostavite%20na%20ovu%20adresu%20u%20digitalnom%20obliku%20." +
-      "%0A%0ALijep%20pozdrav%2C%0A%0A****OVDJE%20NAVEDITE%20IME%20I%20PREZIME%20PRIJE%20SLANJA%20ZAHTJEVA****" +
+    url = "http://imamopravoznati.org/new/imamo_pravo_znati_test2/?title=#{incident.incident_number};" +
+      "body=Za&#58;%20" +
+      "%2C%0A%0APod%20Zakonom%20o%20pravu%20na%20pristup%20informacijama%20molim%20vas%20podatke%20" +
+      "sadrzane%20u%20&#34;#{ incident.incident_number }&#34;&#46;%20Dostavite%20sljede%C4%87e%20" +
+      "informacije%3A%0A%0A%20#{ incident.incident_number }&#46;%20" +
+      "%0A%0ALijep%20pozdrav%2C%0A%0A****OVDJE%20SE%20POTPISI%20PUNIM%20IMENOM%20PRIJE%20SLANJA****" +
       "&tags=" + u("detentionlogs incident-number:#{incident.incident_number}")
     text ||= "Preuzmi #{incident.incident_number}"
-    title =  "Otvara unaprijed popunjeni zahtjev za pristup informacijama kori&#353;tenjem Imamo pravo znati alata"
+    title =  "Otvara unaprijed popunjeni zahtjev za pristup informacijama putem ImamoPravoZnati alata"
     link_to text, url, { title: title, class: "btn btn-submit"}
   end
 
@@ -27,11 +27,11 @@ module IncidentsHelper
   def link_to_right_to_know(incident)
     if (incident.foi_requests.count == 1)
       foi_request = incident.foi_requests.first
-      text = "PPI zahtjev za vi&#353;e detalja: #{foi_request.display_status}"
+      text = "PPI zahtjev za vise detalja: #{foi_request.display_status}"
       link = "http://imamopravoznati.org/request/#{foi_request.url_title}"
     else
       statuses = incident.foi_requests.map(&:display_status).uniq
-      text = "#{incident.foi_requests.length} PPI zahtjev za vi&#353;e detalja: #{statuses.to_sentence.gsub('.', '')}"
+      text = "#{incident.foi_requests.length} PPI zahtjev: #{statuses.to_sentence.gsub('.', '')}"
       link = "http://imamopravoznati.org/search/#{incident.incident_number}"
     end
 
